1146aaee7b8cab60419d652d62538db887ad65ae
Kabuda-czh <634469564@qq.com>
2023-09-18 18:08:51 +0800

    ✨ feat: add new schedule func & fix excel

--- src/common.ts
+++ src/common.ts
@@ -1,21 +1,8 @@
 import * as xlsx from 'excel4node'
 import { LostarkSchedule, Round, ScheduleUser } from './types'
-import { targetDays } from './utils'
-
-const targetDayIndexMap: { [key: string]: number } = {
-  '三': 0,
-  '四': 1,
-  '五': 2,
-  '六': 3,
-  '日': 4,
-  '天': 4,
-  '七': 4,
-  '一': 5,
-  '二': 6,
-}
 
 // 导出 excel 表格
-export async function exportExcel(filePath: string, map: string, mapTeamNumber: number, users: LostarkSchedule[]) {
+export async function exportExcel(filePath: string, map: string, mapTeamNumber: number, users: LostarkSchedule[], isLast = false) {
   return new Promise<void>((resolve, reject) => {
     const workbook = new xlsx.Workbook({
       defaultFont: {
@@ -53,11 +40,10 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
     // 合并单元行
     worksheet.cell(1, 1, 1, 13, true)
     worksheet.cell(2, 2, 2, 4, true)
-    worksheet.cell(2, 5, 2, 11, true)
 
     // 第一行基础信息
     worksheet.cell(1, 1)
-      .string(`${map}周时间待定`)
+      .string(`${map}${isLast ? '上' : '本'}周时间排期表`)
       .style(sheetStyle())
 
     // 第二行基础信息
@@ -70,11 +56,7 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
       .style(colorSheetStyle('#C7ECFF'))
 
     worksheet.cell(2, 5)
-      .string('首班21点15')
-      .style(colorSheetStyle('#FFC9C7'))
-
-    worksheet.cell(2, 12)
-      .string('改了就填这列不然看不见')
+      .string('改了就填这列')
       .style(colorSheetStyle('#A3D5BB'))
 
     // 第三行基础信息
@@ -99,15 +81,7 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
       .string('奶')
       .style(sheetStyle())
 
-    const daysArray = ['周三', '周四', '周五', '周六', '周日', '周一', '周二']
-
-    daysArray.forEach((day, index) => {
-      worksheet.cell(3, 5 + index)
-        .string(day)
-        .style(sheetStyle())
-    })
-
-    worksheet.cell(3, 12)
+    worksheet.cell(3, 5)
       .string('修改日期')
       .style(sheetStyle())
 
@@ -118,26 +92,18 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
         .style(sheetStyle())
 
       worksheet.cell(4 + index, 2)
-        .number(user.dps2)
+        .number(isLast ? user.lastDps2 : user.dps2)
         .style(colorSheetStyle('#FFDCC4'))
 
       worksheet.cell(4 + index, 3)
-        .number(user.dps1)
+        .number(isLast ? user.lastDps1 : user.dps1)
         .style(colorSheetStyle('#F2C7FF'))
 
       worksheet.cell(4 + index, 4)
-        .number(user.mercy)
+        .number(isLast ? user.lastMercy : user.mercy)
         .style(sheetStyle())
 
-      // 根据 days 来打勾
-      targetDays.forEach((day) => {
-        if (user.days[day])
-          worksheet.cell(4 + index, 5 + targetDayIndexMap[day])
-            .string('√')
-            .style(sheetStyle())
-      })
-
-      worksheet.cell(4 + index, 12)
+      worksheet.cell(4 + index, 5)
         .string(user.uploadDate)
         .style(sheetStyle())
     })
@@ -159,8 +125,12 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
       .formula(`SUM(D4:D${4 + users.length})`)
       .style(sheetStyle())
 
+    worksheet.cell(5 + users.length, 5)
+      .formula(`SUM(B${5 + users.length}:D${5 + users.length})`)
+      .style(sheetStyle())
+
     // 排班算法 (每4人一队伍, 每一轮一共2队, 每队最好 2dps1 + 1dps2 + 1奶)
-    const rounds = schedule(users, mapTeamNumber)
+    const rounds = schedule(users, mapTeamNumber, isLast)
 
     rounds.forEach((round, index) => {
       const startRow = 6 + users.length + index * 3
@@ -233,9 +203,6 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
       worksheet.column(i).setWidth(18)
     }
 
-    // 设置第十二列的宽度 24
-    worksheet.column(12).setWidth(24)
-
     workbook.write(filePath, (err, stats) => {
       if (err)
         reject(err)
@@ -246,7 +213,7 @@ export async function exportExcel(filePath: string, map: string, mapTeamNumber: 
 }
 
 // 创建排班
-function schedule(users: LostarkSchedule[], mapTeamNumber = 8): Round[] {
+function schedule(users: LostarkSchedule[], mapTeamNumber = 8, isLast = false): Round[] {
   const rounds: Round[] = []
 
   let pool: LostarkSchedule[] = JSON.parse(JSON.stringify(users))
@@ -254,118 +221,76 @@ function schedule(users: LostarkSchedule[], mapTeamNumber = 8): Round[] {
   // 创建一个工作副本
   pool = [...pool].map(user => ({ ...user, dps1: user.dps1, dps2: user.dps2, mercy: user.mercy }));
 
-  const needMercy = mapTeamNumber / 4, needDps1 = mapTeamNumber / 2, needDps2 = mapTeamNumber / 4, round = mapTeamNumber / 4
+  // 计算所需的队伍轮次
+  let teamNumber = Math.ceil(pool.reduce((p, c) => {
+    p += c.dps1 + c.dps2 + c.mercy
+    return p
+  }, 0) / mapTeamNumber)
 
-  // 主循环：创建完整的轮次队伍
-  while (pool.length >= mapTeamNumber) {
-    const roundUsers: ScheduleUser[] = []
-    const setIds = new Set<string>()
+  const poolMaxValues = pool.map(user => isLast
+    ? Math.max(user.lastDps1, user.lastDps2, user.lastMercy)
+    : Math.max(user.dps1, user.dps2, user.mercy))
 
-    // 按角色筛选和排序玩家
-    let dps1Candidates = pool.filter(p => p.dps1 > 0).sort((a, b) => b.dps1 - a.dps1)
-    let dps2Candidates = pool.filter(p => p.dps2 > 0).sort((a, b) => b.dps2 - a.dps2)
-    let mercyCandidates = pool.filter(p => p.mercy > 0).sort((a, b) => b.mercy - a.mercy)
+  const poolMaxValue = Math.max(...poolMaxValues)
 
-    if (mercyCandidates.length >= needMercy && dps1Candidates.length >= needDps1 && dps2Candidates.length >= needDps2) {
-      // 尝试组成 4人小队
-      for (let i = 0; i < round; i++) {
-        let mercy: LostarkSchedule, dps1_1: LostarkSchedule, dps1_2: LostarkSchedule, dps2: LostarkSchedule
+  teamNumber = teamNumber > poolMaxValue ? teamNumber : poolMaxValue
 
-        // 过滤重复人员
-        mercyCandidates = mercyCandidates.filter(p => !setIds.has(p.qq))
-        if (mercyCandidates.length >= 1) {
-          mercy = mercyCandidates.shift()!
-          setIds.add(mercy.qq)
-          roundUsers.push({ user: mercy, role: 'mercy' })
+  const sortFunc = (a: LostarkSchedule, b: LostarkSchedule) =>
+    isLast
+      ? (b.lastDps1 + b.lastDps2 + b.lastMercy) - (a.lastDps1 + a.lastDps2 + a.lastMercy)
+      : (b.dps1 + b.dps2 + b.mercy) - (a.dps1 + a.dps2 + a.mercy)
 
-          dps1Candidates = dps1Candidates.filter(p => !setIds.has(p.qq))
-          if (dps1Candidates.length >= 2) {
-            dps1_1 = dps1Candidates.shift()!
-            dps1_2 = dps1Candidates.shift()!
-            setIds.add(dps1_1.qq)
-            setIds.add(dps1_2.qq)
-            roundUsers.push({ user: dps1_1, role: 'dps1' })
-            roundUsers.push({ user: dps1_2, role: 'dps1' })
-          } else
-            break
+  const userCandidates = pool.sort(sortFunc)
 
-          dps2Candidates = dps2Candidates.filter(p => !setIds.has(p.qq))
-          if (dps2Candidates.length >= 1) {
-            dps2 = dps2Candidates.shift()!
-            setIds.add(dps2.qq)
-            roundUsers.push({ user: dps2, role: 'dps2' })
-          } else
-            break
-        } else
-          break
+  const needMercy = mapTeamNumber / 4
+
+  const addUserToRound = (newUser: ScheduleUser) => {
+    for (let i = 0; i < rounds.length; i++) {
+      const isUserExist = rounds[i].users.some(user => user.user.qq === newUser.user.qq)
+      const isMercySatisfy = rounds[i].users.filter(user => user.role === 'mercy').length <= needMercy
+      const isLengthSatisfy = rounds[i].users.length < mapTeamNumber
+
+      if (!isUserExist && isMercySatisfy && isLengthSatisfy) {
+        if (newUser.role === 'mercy' && rounds[i].users.filter(user => user.role === 'mercy').length + 1 > needMercy)
+          continue
+        rounds[i].users.push(newUser)
+        return
       }
     }
 
-    // 刷新池
-    pool = pool.filter(p => p.dps1 > 0 || p.dps2 > 0 || p.mercy > 0)
-    // 如果成功创建了一轮次，将其添加到轮次列表中
-    if (roundUsers.length === mapTeamNumber) {
-      roundUsers.map(user => user.user[user.role as 'dps1' | 'dps2' | 'mercy']--)
-      rounds.push({ users: roundUsers })
-    }
-    else
-      break
+    rounds.push({
+      users: [newUser]
+    })
   }
 
-  // 处理剩余玩家
-  while (pool.length > 0) {
-    const roundUsers: ScheduleUser[] = []
+  userCandidates.forEach(user => {
+    const { dps1, dps2, mercy, lastDps1, lastDps2, lastMercy } = user
 
-    const setIds = new Set<string>()
+    const dps1Number = isLast ? lastDps1 : dps1
+    const dps2Number = isLast ? lastDps2 : dps2
+    const mercyNumber = isLast ? lastMercy : mercy
 
-    // 按角色筛选
-    let dpsCandidates = pool.filter(p => p.dps1 > 0 || p.dps2 > 0)
-    let mercyCandidates = pool.filter(p => p.mercy > 0)
-
-    if (mercyCandidates.length > needMercy) {
-      // 如果奶妈人数大于2，优先选择奶妈
-      for (let i = 0; i < needMercy; i++) {
-        const mercy = mercyCandidates.shift()!
-        setIds.add(mercy.qq)
-        roundUsers.push({ user: mercy, role: 'mercy' })
-      }
-    } else {
-      if (mercyCandidates.length) {
-        // 如果奶妈人数小于等于2，全部选择奶妈
-        mercyCandidates.forEach(p => {
-          p.mercy--
-          setIds.add(p.qq)
-          roundUsers.push({ user: p, role: 'mercy' })
-        })
-      }
-    }
-
-    // 过滤人员
-    dpsCandidates = dpsCandidates.filter(p => !setIds.has(p.qq))
-
-    if (dpsCandidates.length > 0) {
-      let count = 0
-      dpsCandidates.forEach(p => {
-        if (count < needDps1 + needDps2) {
-          if (p.dps1)
-            roundUsers.push({ user: p, role: 'dps1' })
-          else if (p.dps2)
-            roundUsers.push({ user: p, role: 'dps2' })
-          count++
-        }
-        else
-          return
+    for (let i = 0; i < mercyNumber; i++) {
+      addUserToRound({
+        user,
+        role: 'mercy'
       })
     }
 
-    if (roundUsers.length) {
-      roundUsers.map(user => user.user[user.role as 'dps1' | 'dps2' | 'mercy']--)
-      rounds.push({ users: roundUsers })
+    for (let i = 0; i < dps1Number; i++) {
+      addUserToRound({
+        user,
+        role: 'dps1'
+      })
     }
 
-    // 刷新池
-    pool = pool.filter(p => p.dps1 > 0 || p.dps2 > 0 || p.mercy > 0)
-  }
+    for (let i = 0; i < dps2Number; i++) {
+      addUserToRound({
+        user,
+        role: 'dps2'
+      })
+    }
+  })
 
   return rounds
 }
